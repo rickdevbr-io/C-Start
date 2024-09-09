6b35e2409fc37a8828f867e09a9e914df9e7b1c6
rickdevbr-io <ricardodeveloper21br@gmail.com>
2024-09-09 17:04:30 -0300

    start project

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿string mensagemDeBoasVindas = "Boas vindas ao Screen Sound.";
+List<string> bandas = new List<string>{"U2","The Beatles"};
+
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+void RegistrarBanda()
+{
+    Console.Clear();
+    ExibirTitulosAsteriscos("Registro de bandas");
+    Console.Write("\nDigite o nome da banda que deseja registrar: ");
+     string nomeDaBanda = Console.ReadLine()!;
+    bandas.Add(nomeDaBanda);
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+    Console.Write("\nDigite sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1:
+            RegistrarBanda();
+            break;
+        case 2:
+            ExibirBandasRegistradas();
+            break;
+        case 3:
+            Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+            break;
+        case 4:
+            Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+            break;
+        case -1:
+            Console.WriteLine("Tchau tchau :)");
+            break;
+        default: 
+            Console.WriteLine("Opção inválida.");
+            break;
+    }
+}
+
+void ExibirBandasRegistradas()
+{
+    Console.Clear();
+    ExibirTitulosAsteriscos("Exibindo todas as bandas registradas");
+    bandas.ForEach(banda =>{
+        Console.WriteLine($"Banda: {banda}");
+    });
+    Console.WriteLine("\nAberte qualquer tecla para voltar ao nemu inicial.");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+void ExibirTitulosAsteriscos( string titulo)
+{
+    int qtdeLetras = titulo.Length;
+    string asteriscos = string.Empty.PadLeft(qtdeLetras, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine($"{asteriscos}\n");
+}
+
+ExibirOpcoesDoMenu();
